9309c0589ebb1f1e443b716df649a6d2820f4219
Hamza <s8mujami@stud-uni.saarland.de>
2023-09-10 17:06:18 +0200

    datebase configuration

--- ruby/app/models/db_init.rb
+++ ruby/app/models/db_init.rb
@@ -1,11 +1,13 @@
 require 'active_record'
+require 'pg'
+
 
 DB = ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
   :database => ENV['DB_NAME'],
   :host => ENV['DB_HOST'],
   :username => ENV['DB_USER'],
-  :password => ENV['DB_PASS'],
+  :password => ENV['DB_PASS']
 )
 
 ActiveRecord::Base.connection.create_table(:articles, primary_key: 'id', force: true) do |t|
